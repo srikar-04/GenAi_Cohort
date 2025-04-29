db5e5d3d5e6e004002b121b543c2d39d92b6de09
srikar <srikar.code01@gmail.com>
2025-04-29 21:44:19 +0530

    vector embedding but exceded openAi limit

--- 01_basics/embeddings.py
+++ 01_basics/embeddings.py
@@ -1,4 +1,18 @@
 from dotenv import load_dotenv
 from openai import OpenAI
+import os
 
 load_dotenv()
+
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+prompt = "what is a blackhole"
+
+response = client.responses.create(
+    model='gpt-3.5-turbo',
+    input=prompt,
+)
+
+print(response.output_text)
