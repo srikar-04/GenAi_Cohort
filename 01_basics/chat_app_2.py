c053cee2da16533e20e51fb4faa86ae5ec3af4c9
srikar <srikar.code01@gmail.com>
2025-05-07 09:40:53 +0530

    refactor: update system prompt and output format for clarity and consistency

--- 01_basics/chat_app_2.py
+++ 01_basics/chat_app_2.py
@@ -4,6 +4,11 @@ import os
 from google import genai
 from google.genai import types
 import json
+from pydantic import BaseModel
+
+class Math_Operation(BaseModel):
+    step: str
+    content: str
 
 api_key = os.environ['GEMINI_API_KEY']
 
@@ -20,43 +25,45 @@ system_prompt = """
     Follow the steps in sequence that is "analyse", "think", "output", "validate" and finally "result".
 
     Rules:
-    1. Follow the strict JSON output as per Output schema.
-    3. Carefully analyse the user query
+    1. Output should be strictly in JSON format.
+    2. Carefully analyse the user query
 
     Output Format:
-    {{ step: "string", content: "string" }}
+    {step: 'step name', content: 'step description' }
 
     Example:
     Input: What is 2 + 2.
-    Output: {{ step: "analyse", content: "Alright! The user is intersted in maths query and he is asking a basic arthermatic operation" }}
-    Output: {{ step: "think", content: "To perform the addition i must go from left to right and add all the operands" }}
-    Output: {{ step: "output", content: "4" }}
-    Output: {{ step: "validate", content: "seems like 4 is correct ans for 2 + 2" }}
-    Output: {{ step: "result", content: "2 + 2 = 4 and that is calculated by adding all numbers" }}
+    Output: { step: "analyse", content: "Alright! The user is intersted in maths query and he is asking a basic arthermatic operation" }
+    Output: { step: "think", content: "To perform the addition i must go from left to right and add all the operands" }
+    Output: { step: "output", content: "4" }
+    Output: { step: "validate", content: "seems like 4 is correct ans for 2 + 2" }
+    Output: { step: "result", content: "2 + 2 = 4 and that is calculated by adding all numbers" }
 
 """
 
-# contents =[
-#     types.Content(
-#         role='system',
-#         parts=types.Part.from_text(text=system_prompt)
-#     ),
+prompt = 'what is 45 * 20'
 
-#     types.Content(
-#         role="user",
-#         parts=types.Part.from_text(text="what is 45 * 20")
-#     )
-# ]
+prompt_1 = "what is life?"
 
 response = client.models.generate_content(
     model='gemini-2.0-flash',
-    contents=['what is 45 * 20'],
+    contents = [prompt],
     config = types.GenerateContentConfig(
         max_output_tokens=500,
         temperature=0.1,
         system_instruction=system_prompt,
-    )
+        response_mime_type="application/json",
+        response_schema=list[Math_Operation]
+    ),    
 )
-# print(json.dumps(response, indent=2))
-# print(response.text)
-print(type(response.text))
+
+try:
+    result = json.loads(response.text)
+    for res in result:
+        if(res["step"] != 'result'):
+            print(f"🧠 {res['content']}")
+        else:
+            print(f"🤖 {res['content']}")
+    # print(result[0]['content'])
+except:
+    print('oops! ai has not given a proper json response !!')
