d8ff385ac3e6f6af67eb63f12145c413d5028b19
srikar <srikar.code01@gmail.com>
2025-05-10 11:20:25 +0530

    complete weather agent without api calling

--- 01_basics/weather_agent.py
+++ 01_basics/weather_agent.py
@@ -3,6 +3,8 @@ load_dotenv()
 from mistralai import Mistral
 import os
 import json
+import functools
+import re
 
 api_key = os.environ["MISTRAL_API_KEY"]
 model = "mistral-small-latest"
@@ -17,7 +19,7 @@ tools = [
     {
         "type": "function",
         "function": {
-            "name": get_weather,
+            "name": "get_weather",
             "description": "Returns the weather in a given location",
             "parameters": {
                 "type": "object",
@@ -30,8 +32,12 @@ tools = [
                 "required": ["city"]
             }
         }
-    }
+    },
 ]
+
+names_to_functions = {
+    "get_weather": functools.partial(get_weather),
+}
 
 system_prompt = """
 You are a helpful AI assistant who is expert in breaking down complex problems and resolving user queries.
@@ -45,8 +51,10 @@ Rules:
 1. Output should be strictly in JSON format.
 2. Strictly perform only one step at a time and wait for the next input.
 3. Carefully analyze the user query.
+4. Donot wrap the response in a code block or fences
 
 Output JSON Format:
+
 {
     "step": "step name",
     "content": "step description",
@@ -67,7 +75,6 @@ Tools:
 - input: "Takes input from the user query"
 """
 
-
 messages = [
     {
         "role": "system",
@@ -75,62 +82,267 @@ messages = [
     },
 ]
 
-user_query = input("Enter Your Query \n >")
+user_query = input("Enter Your Query \n > ")
 
 messages.append(
     {
-        "role":"user",
+        "role": "user",
         "content": user_query
     },
 )
 
 while True:
-
     chat_response = client.chat.complete(
         model=model,
         messages=messages,
         max_tokens=500,
         temperature=0.1,
-        response_format={"type": "json_object"},
         tools=tools,
         tool_choice="auto"
     )
 
     try:
-        result = chat_response.choices[0].message.content
-        parsed_response = json.loads(result)
-        # print(parsed_response)
+        result = chat_response.choices[0].message
+        parsed_response = None
 
-        messages.append({
-            "role": "assistant",
-            "content": json.dumps(parsed_response)
-        })
+        # If the model wants to call a tool/function
+        if result.tool_calls:
+            tool_call = result.tool_calls[0]
+            function_name = tool_call.function.name
+            function_arguments = json.loads(tool_call.function.arguments)
 
-        # handling execute step where we need to do function call
-        if parsed_response.get("step") == "execute":
-            print(f"🧠: {parsed_response.get("content")}")
-            tool_name = parsed_response.get("function")
-            tool_input = parsed_response.get("input")
+            # First add the assistant's message with the tool call
+            # messages.append({
+            #     "role": "assistant",
+            #     "content": None,
+            #     "tool_calls": result.tool_calls
+            # })
 
-            if tools[0].get("function", {}).get("name") == tool_name:
-                output = tools[0].get("function", {}).get("name")(tool_input)
+            if function_name == tools[0].get("function").get("name"):
+                output = names_to_functions.get(function_name)(**function_arguments)
                 messages.append(
                     {
-                        "role": "assitant",
-                        "content": json.dumps({"step": "monitor", "output": output})
+                        "role": "tool",
+                        "tool_call_id": tool_call.id,
+                        "name": function_name,
+                        "content": output
                     }
                 )
-            else:
-                print(f"😱: function name did not match")
             continue
 
-        # handling result/final step
-        if parsed_response.get("step") == "result":
-            print(f"🤖: {parsed_response.get("content")}")
-            break
-        print(f"🧠: {parsed_response.get("content")}")
+        else:
+            try:
+                parsed_response = json.loads(result.content)
+            except json.JSONDecodeError:
+                # print("JSON messed up")
+                reg_expression = re.sub(r"```json|```", "", result.content)
+                parsed_response = json.loads(reg_expression)
+                
+            messages.append({
+                "role": "assistant",
+                "content": result.content
+            })
+
+            messages.append({
+                "role": "user",
+                "content": "Please continue with the next step"
+            })
+            
+
+            print(f"🧠: {parsed_response.get('content')}")
+
+            if parsed_response.get("step") == "result":
+                print(f"🤖: {parsed_response.get('content')}")
+                break
 
     except Exception as e:
         print(f"Something went wrong!!:  {e}")
 
-    # print(chat_response.choices[0].message.content)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from dotenv import load_dotenv
+# load_dotenv()
+# from mistralai import Mistral
+# import os
+# import json
+# import functools
+# import re
+
+# api_key = os.environ["MISTRAL_API_KEY"]
+# model = "mistral-small-latest"
+
+# client = Mistral(api_key=api_key)
+
+
+# def get_weather(city: str) -> str:
+#     return f"weather in {city} is 31 degree celcius!!"
+
+# tools = [
+#     {
+#         "type": "function",
+#         "function": {
+#             "name": "get_weather",
+#             "description": "Returns the weather in a given location",
+#             "parameters": {
+#                 "type": "object",
+#                 "properties": {
+#                     "city": {
+#                         "type": "string",
+#                         "description": "The city to get the weather for"
+#                     }
+#                 },
+#                 "required": ["city"]
+#             }
+#         }
+#     },
+# ]
+
+# # function_map = {
+# #     "get_weather": get_weather
+# # }
+
+# names_to_functions = {
+#     "get_weather": functools.partial(get_weather),
+# }
+
+# system_prompt = """
+# You are a helpful AI assistant who is expert in breaking down complex problems and resolving user queries.
+# You work in the following modes: start, plan, execute, monitor, and result.
+# For the given user query and available tools, plan step-by-step execution. Based on planning, select the relevant tool from the available tools. Perform the task based on the selected tool.
+# Wait for observation and resolve the user query based on the observation from the tool call.
+
+# Follow the steps in sequence: "start", "plan", "execute", "monitor", and "result".
+
+# Rules:
+# 1. Output should be strictly in JSON format.
+# 2. Strictly perform only one step at a time and wait for the next input.
+# 3. Carefully analyze the user query.
+# 4. Donot wrap the response in a code block or fences
+
+# Output JSON Format:
+
+# {
+#     "step": "step name",
+#     "content": "step description",
+#     "function": "name of function specified in Tools section. Specify it only if the step is 'execute'",
+#     "input": "input to the function present in user query. Specify it only if the step is 'execute'"
+# }
+
+# Example:
+# User Query: What is the weather in New York?
+# - {"step": "start", "content": "Alright! The user is interested in a weather query and is asking about the weather in New York"}
+# - {"step": "plan", "content": "To get the weather in New York, I need to call the get_weather function"}
+# - {"step": "execute", "function": "get_weather", "input": "New York"}
+# - {"step": "monitor", "content": "Weather in New York is very hot today, 31 degrees Celsius."}
+# - {"step": "result", "content": "31 degrees Celsius."}
+
+# Tools:
+# - get_weather: "Returns the weather in a given location"
+# - input: "Takes input from the user query"
+# """
+
+
+# messages = [
+#     {
+#         "role": "system",
+#         "content": system_prompt
+#     },
+# ]
+
+# user_query = input("Enter Your Query \n > ")
+
+# messages.append(
+#     {
+#         "role":"user",
+#         "content": user_query
+#     },
+# )
+
+# while True:
+
+#     chat_response = client.chat.complete(
+#         model=model,
+#         messages=messages,
+#         max_tokens=500,
+#         temperature=0.1,
+#         # response_format={"type": "json_object"},
+#         tools=tools,
+#         tool_choice="auto"
+#     )
+
+#     try:
+#         result = chat_response.choices[0].message
+#         parsed_response = None
+
+#         #handling if there is any tool call
+#         if result.tool_calls:
+#             tool_call = result.tool_calls[0]
+#             function_name = tool_call.function.name
+#             function_arguments = json.loads(tool_call.function.arguments)
+
+#             parsed_response = {
+#                 "step": "execute",
+#                 "content": f"Calling {function_name} with {function_arguments}",
+#                 "function": function_name,
+#                 "input": function_arguments.get("city")
+#             }
+#         else:
+#             try:
+#                 parsed_response = json.loads(result.content)
+#             except json.JSONDecodeError:
+#                 print("JSON fucked up")
+#                 parsed_response = json.loads(re.sub(r"```json|```", "", result.content))
+#                 print(parsed_response)
+
+
+#         # handling execute step where we need to do function call
+#         if parsed_response.get("step") == "execute":
+#             print(f"🧠: {parsed_response.get("content")}")
+#             tool_name = parsed_response.get("function")
+#             tool_input = parsed_response.get("input")
+
+#             if tools[0].get("function", {}).get("name") == tool_name:
+#                 output = names_to_functions.get(tool_name)(tool_input)
+#                 messages.append(
+#                     {
+#                         "role": "assitant",
+#                         "content": json.dumps({"step": "monitor", "output": output})
+#                     }
+#                 )
+#             else:
+#                 print(f"😱: function name did not match")
+#             continue
+
+#         # handling result/final step
+#         if parsed_response.get("step") == "result":
+#             print(f"🤖: {parsed_response.get("content")}")
+#             break
+
+#         messages.append({
+#             "role": "assistant",
+#             "content": json.dumps(parsed_response)
+#         })
+
+#         print(f"🧠: {parsed_response.get("content")}")
+
+#     except Exception as e:
+#         print(f"Something went wrong!!:  {e}")
