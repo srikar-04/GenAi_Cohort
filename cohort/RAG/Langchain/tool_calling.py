6838c7015e34e46549899e0ec746c89aab72e86f
srikar <srikar.code01@gmail.com>
2025-05-26 17:34:58 +0530

    added multiple function calling ability with actual apis

--- cohort/RAG/Langchain/tool_calling.py
+++ cohort/RAG/Langchain/tool_calling.py
@@ -3,7 +3,8 @@ load_dotenv()
 import os
 from langchain.chat_models import init_chat_model
 from langchain_core.tools import tool
-import json
+import requests
+import subprocess
 
 openai_api_key = os.environ["OPENAI_API_KEY"]
 
@@ -14,7 +15,14 @@ def get_weather(location:str)->str:
         Args:
         [location]
     """
-    return f"current weather in {location} is 31 degrees"
+    print(f"⛏️: running get weather function")
+    url = f"https://wttr.in/{location}?format=%C+%t"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return f"weather in {location} is {response.text}"
+    else:
+        return "something went wrong with weather api"
 
 @tool
 def run_command(command):
@@ -23,35 +31,90 @@ def run_command(command):
         Args:
         [command]
     """
-    return "command ran succesfully"
+    print(f"⛏️: running execute command function !!!!")
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    print(result)
+    if result.returncode == 0:
+        return f"Command executed successfully:\n{result.stdout.strip()}"
+    else:
+        raise ValueError("Execute command function failed!!")
 
 available_tools = {
     "get_weather": get_weather,
     "run_command": run_command,
 }
 
-llm = init_chat_model("gpt-4o-mini", model_provider="openai")
-
 tools = [get_weather, run_command]
 
+system_prompt = f"""
+    you are an helpful ai assistant.
+    you can resole user queries and answer based with availbe tools you have
+    Here are the available list of tools
+    Available Tools:
+    {available_tools}
+
+    Responses from specific tool calls are added to "messages" list
+    Based on that list give me the final answer
+"""
+
+messages = [
+    {
+        "role": "developer",
+        "content": system_prompt
+    }
+]
+
+llm = init_chat_model("gpt-4o-mini", model_provider="openai")
 llm_with_tools = llm.bind_tools(tools)
 
-user_query = input(">> ")
+while True:
+    user_query = input(">> ")
 
-ai_message = llm_with_tools.invoke([user_query])
+    messages.append(
+        {
+            "role": "user",
+            "content": user_query
+        }
+    )
 
-print(f"ACTUAL TOOL CALL RESPONSE : {ai_message.tool_calls}")
+    ai_message = llm_with_tools.invoke([user_query])
+    # messages.append(
+    #     {
+    #         "role":"assistant",
+    #         "content": ai_message
+    #     }
+    # )
 
-if ai_message.tool_calls:
-    name = ai_message.tool_calls[0].get("name")
-    function_name = available_tools.get(name)
+    print(f"ACTUAL TOOL CALL RESPONSE : {ai_message.tool_calls}")
 
-    function_args = ai_message.tool_calls[0].get("args")
-    
-    print(f'FUNCTION_NAME: {function_name} \n FUNCTION_ARGUMENTS: {function_args}')
-    print(f"TYPE OF ARGS: {type(function_args)}")
+    if ai_message.tool_calls:
+        for tool in ai_message.tool_calls:
+            name = tool.get("name")
+            function_name = available_tools.get(name)
 
-    tool_response = function_name.invoke(function_args)
-    print(f'THIS IS THE RESPONSE FROM FUNCTION: {tool_response}')
-else:
-    print("OOPS! NO TOOL CALL PRESENT")
+            function_args = tool.get("args")
+
+            # print(f'FUNCTION_NAME: {function_name} \n FUNCTION_ARGUMENTS: {function_args}')
+            # print(f"TYPE OF ARGS: {type(function_args)}")
+
+            tool_response = function_name.invoke(function_args)
+            print(f'THIS IS THE RESPONSE FROM FUNCTION: {tool_response}')
+            messages.append(
+                {
+                    "role": "assistant",
+                    "content": tool_response
+                }
+            )
+    else:
+        print("OOPS! NO TOOL CALL PRESENT")
+        # print(f"NO TOOL RESPONSE: {ai_message.content}")
+        messages.append(
+            {
+                "role": "assistant",
+                "content": ai_message.content
+            }
+        )
+
+    response = llm.invoke(messages)
+
+    print(f"FINAL RESPONSE 🤖 : {response.content}")
