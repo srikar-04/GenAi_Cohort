111a0dea316c4da147137bcc2ba439b2fcef8a23
srikar <srikar.code01@gmail.com>
2025-06-13 21:43:55 +0530

    query rewriting using chat completion api

--- cohort/Gemini/RAG/Query_Transformation/query_rewriting.py
+++ cohort/Gemini/RAG/Query_Transformation/query_rewriting.py
@@ -2,30 +2,39 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 from openai import OpenAI
+import json
 
 gemini_api_key = os.environ["GEMINI_API_KEY"]
 gemini_model = "gemini-2.0-flash"
 
+client = OpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
 
 def rewrite_query(query):
     return f"THIS QUERY IS TRANSFORMED {query}"
 
+
 tools = [{
     "type": "function",
-    "name": "rewrite_query",
-    "description": "Rewrites the original query to add more context for better response.",
-    "parameters": {
-        "type": "object",
-        "properties": {
-            "query": {
-                "type": "string",
-                "description": "Original query sent by the user which needs to be transformed."
-            }
+    "function": {
+        "name": "rewrite_query",
+        "description": "Rewrites the original query to add more context for better response.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "query": {
+                    "type": "string",
+                    "description": "Original query sent by the user which needs to be transformed."
+                }
+            },
+            "required": [
+                "query"
+            ],
+            "additionalProperties": False
         },
-        "required": [
-            "query"
-        ],
-        "additionalProperties": False
+        "strict": True
     }
 }]
 
@@ -33,14 +42,20 @@ available_tools = {
     "rewrite_query": rewrite_query,
 }
 
-client = OpenAI(
-    api_key=gemini_api_key,
-    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
-)
 
 system_prompt = f"""
-    You are an helpfull ai assistant, trained in answering user queries by modifying them so that the original query has more context to generate greate responses.
+    You are an helpfull ai assistant, trained in answering user queries by applying first principle thinking. You are given with some tools to modify user query so that the original query has more context to generate great responses.
+
     This original query rewriting can be done by choosing the appropriate tools you are provided with.
+
+    Here are the steps involved: 
+    step-1: analyse the user query and see if it is more abstract and if there is any actual need for query rewriting.
+    step-2: if you think there is a need then call appropriate tool.
+
+    RULES: 
+    - Donot hellucinate anyting.
+    - If there is a need for query rewriting then pass the exact or original query written by the user.
+    - Donot modify original query before tool calling
 
     Available tools are given below
     {available_tools}
@@ -61,10 +76,32 @@ messages.append(
     }
 )
 
-response = client.responses.create(
+response = client.chat.completions.create(
     model= gemini_model,
-    input = messages,
+    messages = messages,
     tools=tools,
 )
 
-print(response)
+# print(response.choices[0].message.content)
+# print(response.choices[0].message)
+
+result = response.choices[0].message
+
+if result.tool_calls:
+    print(f"TOOL CALL IS PRESENT")
+    # print(result.tool_calls)
+    name = result.tool_calls[0].function.name
+    args = json.loads(result.tool_calls[0].function.arguments)
+
+    print(f"FUNCTION NAME: {name} \n FUNCTION ARGUMENTS: {args}")
+
+    function_name = available_tools.get(name)
+
+    if function_name:
+        function_response = function_name(**args)
+        print(f"FINAL RESPONSE 🤖: {function_response}")
+    else:
+        print("Function is unavailable!!!")
+else:
+    print("NOT TOOL CALL PRESENT, ONLY CONTENT: ")
+    print(result.content)
