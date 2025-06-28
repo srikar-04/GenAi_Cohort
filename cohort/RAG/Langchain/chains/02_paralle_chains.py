4dbe7235c365742a937ed450281e70e695a441e9
srikar-04 <srikar.code01@gmail.com>
2025-06-28 10:03:27 +0530

    parallel chaining in langchain

---
+++ cohort/RAG/Langchain/chains/02_paralle_chains.py
@@ -0,0 +1,57 @@
+from dotenv import load_dotenv
+load_dotenv()
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+
+if "GOOGLE_API_KEY" not in os.environ and "GEMINI_API_KEY" in os.environ:
+    os.environ["GOOGLE_API_KEY"] = os.environ["GEMINI_API_KEY"]
+
+model1 = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+
+model2 = ChatOpenAI()
+
+prompt1 = PromptTemplate(
+    template="""Give me a 5 points summary on the following topic
+        {topic}
+    """,
+    input_variables=["topic"]
+)
+
+prompt2 = PromptTemplate(
+    template="""
+    create 5 or more questions and answers on the following topic
+    {topic}
+""",
+input_variables=["topic"]
+)
+
+prompt3 = PromptTemplate(
+    template="""
+    combine both summary and quiz into single result
+    summary -> {summary}, quiz -> {quiz}
+""",
+input_variables=["summary", "quiz"]
+)
+
+parser = StrOutputParser()
+
+parallel_chain = (
+    {
+        "summary": prompt1 | model1 | parser,
+        "quiz": prompt2 | model2 | parser
+    }
+)
+
+merge_chain = prompt3 | model1 | parser
+
+final_chain = parallel_chain | merge_chain
+
+result = final_chain.invoke({
+    "topic": "cricket"
+})
+
+print(result)
