5f3aee5df7a5dc52b428cf076beedce78c145620
srikar-04 <srikar.code01@gmail.com>
2025-07-01 17:30:42 +0530

    loading and storing pdf file into qdrant db

--- cohort/Gemini/RAG/Query_Transformation/multi_query_generation.py
+++ cohort/Gemini/RAG/Query_Transformation/multi_query_generation.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 load_dotenv()
 import os
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser, PydanticOutputParser
 from langchain_core.runnables import RunnableParallel
@@ -9,8 +9,18 @@ from langchain_core.tools import tool
 import json
 from pydantic import BaseModel, Field
 
+from pathlib import Path
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import pickle
+from langchain_openai import OpenAIEmbeddings
+from qdrant_client import QdrantClient 
+from langchain_qdrant import QdrantVectorStore
+
 if "GOOGLE_API_KEY" not in os.environ and "GEMINI_API_KEY" in os.environ:
     os.environ["GOOGLE_API_KEY"] = os.environ["GEMINI_API_KEY"]
+
+openai_api_key = os.environ["OPENAI_API_KEY"]
 
 model = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
 
@@ -84,6 +94,69 @@ available_tools = {
     "multi_query": multi_query
 }
 
+# PDF LOADING AND TEXT SPLITTING 
+
+cache_path = Path("docs_cache.pkl")
+
+if cache_path.exists():
+    with open(cache_path, "rb") as f:
+        docs = pickle.load(f)
+else:
+    file_path = Path(__file__).parent / "_OceanofPDF.com_AI_Engineering_Building_Applications_-_Chip_Huyen.pdf"
+    # Initialization
+    loader = PyPDFLoader(file_path)
+    # Loading
+    docs = loader.load()
+    with open(cache_path, "wb") as f:
+        pickle.dump(docs, f)
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200,
+)
+
+splitted_text = text_splitter.split_documents(docs)
+
+embed = OpenAIEmbeddings(
+    model="text-embedding-3-large",
+    api_key=openai_api_key
+)
+
+print(f"SPLITTED TEXT : {len(splitted_text)}")
+
+# embed = GoogleGenerativeAIEmbeddings(
+#     model="gemini-embedding-exp-03-07"
+# )
+
+URL = "http://localhost:6333"
+COLLECTION = "AI-engineering-rag"
+
+qclient = QdrantClient(url=URL)
+
+collections = [c.name for c in qclient.get_collections().collections]
+
+need_ingestion = (
+    (COLLECTION not in collections ) or (qclient.count(collection_name=COLLECTION).count == 0)
+)
+
+if need_ingestion:
+    qdrant_vector_store = QdrantVectorStore.from_documents(
+        documents=splitted_text,
+        embedding=embed,
+        collection_name = COLLECTION,
+        url = URL,
+    )
+else:
+    print(f"⏭ Skipping ingest; {COLLECTION} already has data")
+
+qdrant_vector_store = QdrantVectorStore(
+    client=qclient,
+    collection_name=COLLECTION,
+    embedding=embed
+)
+
+retriever = qdrant_vector_store.as_retriever()
+
 system_prompt = PromptTemplate.from_template(
     """
         You are an intelligent ai assistant expert in analysing user queries.
@@ -103,6 +176,7 @@ system_prompt = PromptTemplate.from_template(
 model_with_tools = model.bind_tools(tools)
 
 while True:
+    relevant_docs = []
     try:
         user_query = input("> ")
         if not user_query.strip():
@@ -136,6 +210,20 @@ while True:
 
         # print('TOOL RESPONSE TYPE : ', type(tool_response))  # CONFIRMED THAT THIS IS OF LIST TYPE
 
-        print(f'🤖: {tool_response}')
+        print(f'🤖: {tool_response} {len(tool_response)} \n \n \n \n ')
+
+        for query in tool_response:
+            docs = retriever.invoke(query)
+            relevant_docs.append(docs)
+
+        # print(f"RELEVANT DOCS: {relevant_docs[:5]}")
+        for docs in relevant_docs:
+            # print(f"{docs} \n \n \n \n ")
+            # print( f'{index}. {doc.page_content}' for index, doc in enumerate(docs) )
+            # print(doc for doc in docs)
+            context = "\n\n---\n\n".join(doc.page_content for doc in docs)
+
+        print("THIS IS THE FINAL CONTEXT : 💀", context)
+
     else :
         print(f'🤖: {result.content}')
