d6f0f31fa857a7ae64477171697a1306da4daf07
srikar-04 <srikar.code01@gmail.com>
2025-07-05 20:59:06 +0530

    giving a collection name to batch of chunks but very time taking

--- cohort/Gemini/RAG/Query_Transformation/query_routing_document.py
+++ cohort/Gemini/RAG/Query_Transformation/query_routing_document.py
@@ -1,5 +1,6 @@
-from typing import Literal
+from typing import List, Literal
 from dotenv import load_dotenv
+import langchain_core
 load_dotenv()
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
@@ -8,7 +9,7 @@ from langchain_core.output_parsers import StrOutputParser, PydanticOutputParser
 from langchain_core.runnables import RunnableParallel
 from langchain_core.tools import tool
 import json
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, RootModel
 
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
@@ -20,6 +21,7 @@ from langchain_qdrant import QdrantVectorStore
 
 from langchain_community.retrievers import BM25Retriever
 from langchain_openai import ChatOpenAI
+import math
 
 
 if "GOOGLE_API_KEY" not in os.environ and "GEMINI_API_KEY" in os.environ:
@@ -51,7 +53,13 @@ text_splitter = RecursiveCharacterTextSplitter(
 
 splitted_text = text_splitter.split_documents(docs)
 
-print(f"SPLITTED TEXT TYPE {type(splitted_text)}")
+# print(f"SPLITTED TEXT TYPE {type(splitted_text[0])}")
+# print(f"SPLITTED TEXT TYPE {splitted_text[0]}")
+
+splitted_text_page_content = [content.page_content for content in splitted_text]
+
+# print(f"SPLITTED TEXT : {splitted_text_page_content[0]}")
+print(f"SPLITTED TEXT : {type(splitted_text_page_content[0])}")
 
 embed = OpenAIEmbeddings(
     model="text-embedding-3-large",
@@ -62,17 +70,24 @@ class CollectionName(BaseModel):
     collection_name: str = Field('meaningful collection name based on the context of the text')
     splitted_text: str = Field('text splitted using text splitter, categorizing under a collection name.')
 
+class CollectionChunkList(RootModel[List[CollectionName]]):
+    pass
+
 system_prompt_collection = """
     You are an intelligent AI document categorizer. For each given text chunk, analyze the content deeply and assign a meaningful "collection_name" or category based on the topic or concept covered.
+
+    At the end, after assigning proper names to every chunk, return all the chunks in a list
 
     Guidelines:
     - Create only 3-4 major categories overall to keep collections manageable.
     - Choose intuitive, short collection names (eg: 'AI Basics', 'ML Algorithms', 'Data Engineering', 'LLMs').
     - Do NOT hallucinate or generate irrelevant categories.
     - IMPORTANT: Donot create more than 4 collections.
+    - return python list at the end.
 """
 
-collection_output_parser = PydanticOutputParser(pydantic_object=CollectionName)
+collection_output_parser = PydanticOutputParser(pydantic_object=CollectionChunkList)
+
 
 collection_prompt = ChatPromptTemplate.from_messages([
     ("system", system_prompt_collection),
@@ -82,22 +97,31 @@ collection_prompt = ChatPromptTemplate.from_messages([
 
 collection_chain = collection_prompt | model | collection_output_parser
 
-collection_result_dict = {}
+BATCH_SIZE = 50
+total_chunks = len(splitted_text_page_content)
+batches = math.ceil(total_chunks / BATCH_SIZE)
 
-for chunk in splitted_text:
-    collection_result = collection_chain.invoke(
+print(f"NO OF BATCHES : {batches}")
+
+results = []
+
+for i in range(3):
+    start = i * BATCH_SIZE
+    end = min((i+1)*BATCH_SIZE, total_chunks)
+    chunk_batch = splitted_text_page_content[start:end]
+
+    # chunk_text = "\n \n".join(chunk.page_content for chunk in chunk_batch)
+
+    chunk_result = collection_chain.invoke(
         {
-            "chunk": chunk,
+            "chunk": chunk_batch,
             "format_instructions": collection_output_parser.get_format_instructions()
         }
     )
-    # de-duplicating the splitted text
-    key = collection_result.splitted_text
-    if key not in collection_result_dict:
-        collection_result_dict[key] = collection_result
 
-collection_result_unique = list(collection_result_dict.values())
+    print(f"THIS IS THE TYPE OF CHUNK RESULT IN THE LOOP : {type(chunk_result)}")
+    results.extend(chunk_result.root)
 
-    
-print(f"COLLECTION RESULT TYPE: {collection_result_unique}")
-print(f"COLLECTION RESULT TYPE Lenght: {len(collection_result_unique)}")
+print(f"THIS IS THE TYPE OF FINAL RESULTS TYPE : {type(results)}")
+
+print(results[0])
