42ef14a74bed4ca43f4275b742cb5e0d2bfa38e9
srikar-04 <srikar.code01@gmail.com>
2025-07-08 12:16:58 +0530

    feat: create embeddings of collection names, docs and mapp them

--- cohort/Gemini/RAG/Query_Transformation/query_routing_document_simple.py
+++ cohort/Gemini/RAG/Query_Transformation/query_routing_document_simple.py
@@ -2,6 +2,7 @@ from typing import List, Literal
 from dotenv import load_dotenv
 import langchain_core
 from sklearn.metrics import pairwise_distances_argmin_min
+from sklearn.metrics.pairwise import cosine_similarity
 load_dotenv()
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
@@ -62,156 +63,32 @@ embed = OpenAIEmbeddings(
     api_key=openai_api_key
 )
 
-class CollectionName(BaseModel):
-    collection_name: str = Field('collection name based on the avaible summary text of the content')
+# class CollectionName(BaseModel):
+#     collection_name: str = Field('collection name based on the avaible summary text of the content')
 
-collection_output_parser = PydanticOutputParser(pydantic_object=CollectionName)
+# collection_output_parser = PydanticOutputParser(pydantic_object=CollectionName)
 
 # 1. EMBEDDING THE SPLITTED TEXT : 
-embeddings  = embed.embed_documents([content.page_content for content in splitted_text])
+docs_embeddings  = embed.embed_documents([content.page_content for content in splitted_text])
 
-print('CREATED EMBEDDINGS ✅')
+print('CREATED DOCS EMBEDDINGS ✅')
 
-# 2. CLUSTER THE EMBEDDINGS : 
-kmeans = KMeans(n_clusters=4, random_state=42)
-cluster_ids = kmeans.fit_predict(embeddings)
+# 2. PREDEFINED COLLECTION NAMES AND ITS EMBEDDINGS : 
+collection_names = ["model-adaptation-and-application-development", "model-optimization", "foundation-models", "synthetic-data-strategies"]
 
-print('CREATED EMBEDDINGS CLUSTERS AND OBTAINED IDS ✅')
+collection_names_embeddigs = embed.embed_documents(collection_names)
+print('CREATED COLLECTION NAMES EMBEDDINGS ✅')
 
-# 3. MAPPING CHUNKS TO CLUSTER IDS
-clustered_chunks = {}
-for idx, cluster_id in enumerate(cluster_ids):
-    clustered_chunks.setdefault(cluster_id, []).append(splitted_text[idx])
-print('MAPPED SPLITTED TEXT TO SPECIFIC CLUSTERS ✅')
+collection_doc_map = {}
 
-collection_prompt = PromptTemplate(
-    template= """
-        Generate a short collection name for the following texts: {summary_text}
-        Generate only a single collection name and do not hellucinate.
+for doc in docs_embeddings:
+    similarity = [cosine_similarity([doc], [collection]) for collection in collection_names_embeddigs]
+    max_index = similarity.index(max(similarity))
+    key = collection_names[max_index]
+    # print('COLLECTION CREATED ☑️')
+    if  key in collection_doc_map.keys():
+        collection_doc_map[key].extend(doc)
+    else:
+        collection_doc_map[key] = doc
 
-        example -1:
-
-        HUMAN : 
-
-        summary_text : "Finetuning is the process of adapting a model to a specific task by further
-        training the whole model or part of the model."
-
-        AI RESPONSE : 
-        {{
-            "collection_name": "fine-tuning"
-        }}
-
-        example -2 :
-        summary_text: " Prompt engineering refers to the process of crafting an instruction that gets
-        a model to generate the desired outcome."
-
-        AI RESPONSE : 
-        {{
-            "collection_name": "prompt-engineering"
-        }}
-        
-        Rules : 
-        1. Do not generate multiple collection names.
-        2. Do not hellucinate and follow the instructions
-        3. Work according to the pydantic model.
-        4. IMPORTANT : Repond only in json format.
-    """,
-    input_variables=["summary_text"]
-)
-
-collection_chain = collection_prompt | model | collection_output_parser
-
-cluster_chunk_names = {}
-
-# GETTING THE DOC CLOSEST TO CENTROID TO DECIDE COLLECTION NAME 
-# 4. GENERATING A COLLECTION NAME FOR USING THE CLUSTER :
-
-for cluster_id, doc_in_cluster in clustered_chunks.items():
-    # converting docs in cluster to embeddings : 
-    cluster_embeddings = [embed.embed_query(doc.page_content) for doc in doc_in_cluster] 
-    print("EMBEDDING INSIDE LOOP FOR {cluster_id} ☑️")
-
-    # finding doc closest to cluster centroid : 
-    closest_idx, _ = pairwise_distances_argmin_min(
-        [kmeans.cluster_centers_[cluster_id]], cluster_embeddings
-    )
-    representative_doc = doc_in_cluster[closest_idx[0]]
-    print(f"FOUND REPRESENTATIVE DOC FOR {cluster_id} ☑️")
-
-    summary_text = representative_doc.page_content
-
-    cluster_name = collection_chain.invoke({
-        "summary_text": summary_text
-    })
-
-    cluster_chunk_names[cluster_name.collection_name] = doc_in_cluster
-
-print(cluster_chunk_names)
-
- 
-# for cluster_id, doc_in_cluster in clustered_chunks.items():
-#     summary_text = "\n \n".join(doc.page_content for doc in doc_in_cluster)
-#     cluster_name = collection_chain.invoke({
-#         "summary_text": summary_text
-#     })
-#     print(f"CLUSTER {cluster_id} -> {cluster_name.collection_name}")
-
-# class CollectionName(BaseModel):
-#     collection_name: str = Field('meaningful collection name based on the context of the text')
-#     splitted_text: str = Field('text splitted using text splitter, categorizing under a collection name.')
-
-# class CollectionChunkList(RootModel[List[CollectionName]]):
-#     pass
-
-# system_prompt_collection = """
-#     You are an intelligent AI document categorizer. For each given text chunk, analyze the content deeply and assign a meaningful "collection_name" or category based on the topic or concept covered.
-
-#     At the end, after assigning proper names to every chunk, return all the chunks in a list
-
-#     Guidelines:
-#     - Create only 3-4 major categories overall to keep collections manageable.
-#     - Choose intuitive, short collection names (eg: 'AI Basics', 'ML Algorithms', 'Data Engineering', 'LLMs').
-#     - Do NOT hallucinate or generate irrelevant categories.
-#     - IMPORTANT: Donot create more than 4 collections.
-#     - return python list at the end.
-# """
-
-# collection_output_parser = PydanticOutputParser(pydantic_object=CollectionChunkList)
-
-
-# collection_prompt = ChatPromptTemplate.from_messages([
-#     ("system", system_prompt_collection),
-#     ("human", '{chunk}'),
-#     ("system", "{format_instructions}")
-# ])
-
-# collection_chain = collection_prompt | model | collection_output_parser
-
-# BATCH_SIZE = 50
-# total_chunks = len(splitted_text_page_content)
-# batches = math.ceil(total_chunks / BATCH_SIZE)
-
-# print(f"NO OF BATCHES : {batches}")
-
-# results = []
-
-# for i in range(3):
-#     start = i * BATCH_SIZE
-#     end = min((i+1)*BATCH_SIZE, total_chunks)
-#     chunk_batch = splitted_text_page_content[start:end]
-
-#     # chunk_text = "\n \n".join(chunk.page_content for chunk in chunk_batch)
-
-#     chunk_result = collection_chain.invoke(
-#         {
-#             "chunk": chunk_batch,
-#             "format_instructions": collection_output_parser.get_format_instructions()
-#         }
-#     )
-
-#     print(f"THIS IS THE TYPE OF CHUNK RESULT IN THE LOOP : {type(chunk_result)}")
-#     results.extend(chunk_result.root)
-
-# print(f"THIS IS THE TYPE OF FINAL RESULTS TYPE : {type(results)}")
-
-# print(results[0])
+print(collection_doc_map.keys())
