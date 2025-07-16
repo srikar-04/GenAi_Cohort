947ce2b2ed80f9d00abb540e400377523508dd78
srikar-04 <srikar.code01@gmail.com>
2025-07-16 21:37:35 +0530

    complete dynamic orchestrator workflow

--- cohort/LangGraph/06_orchestrator_worker_workflow.py
+++ cohort/LangGraph/06_orchestrator_worker_workflow.py
@@ -47,3 +47,63 @@ class State(TypedDict):
 class WorkerState(TypedDict):
     section: Section
     completed_sections: Annotated[list, operator.add]
+
+
+def orchestrator(state: State):
+    """create multiple sections based on the given topic (using structured model)"""
+    response = model_with_structured_output.invoke(
+        [
+            SystemMessage(content='create list of sections based on the given topic'),
+            HumanMessage(content=f'Here is the topic based on which you should create different sections : {state["topic"]}')
+        ]
+    )
+    return {"sections": response.sections}
+
+def llm_call(state: WorkerState):
+    """Generate brief description on what all topic should one cover in that particular section"""
+    response = model.invoke(
+        [
+            SystemMessage(content='Generate a reponse on what all are the important topics that i should conver for a given section'),
+            HumanMessage(content=f"This is the section that you should explain with all important topics to cover: \n  SECTION NAME : {state['section'].name} \n SECTION'S DESCRIPTION : {state['section'].description}")
+        ]
+    )
+
+    return {'completed_sections': [response.content]}
+
+def synthesizer(state: State):
+    """Synthesize all the data collected from parallel llm calls"""
+    final_response = '\n \n'.join(content for content in state['completed_sections'])
+    # print(f'THIS IS THE FINAL RESPONSE IN SYNTEHSIZER : {final_response}')
+
+    return {'final_response': final_response}
+
+def assign_worker(state: State):
+    return [Send('llm_call', {"section": s}) for s in state['sections']]
+
+
+graph_builder = StateGraph(State)
+
+# add nodes
+graph_builder.add_node('orchestrator', orchestrator)
+graph_builder.add_node('llm_call', llm_call)
+graph_builder.add_node('synthesizer', synthesizer)
+graph_builder.add_node('assign_worker', assign_worker)
+
+# add edges
+
+graph_builder.add_edge(START, 'orchestrator')
+graph_builder.add_conditional_edges(
+    'orchestrator',
+    assign_worker,
+    ['llm_call']
+)
+graph_builder.add_edge('llm_call', 'synthesizer')
+graph_builder.add_edge('synthesizer', END)
+
+graph = graph_builder.compile()
+
+user_query = input('> ')
+
+final_state = graph.invoke({'topic': user_query})
+
+print(final_state['final_response'])
