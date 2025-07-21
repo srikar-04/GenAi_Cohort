2307ef48b6d7b3a27d4916c1d0e42df364f166f6
srikar-04 <srikar.code01@gmail.com>
2025-07-21 09:26:18 +0530

    created defined nodes and added them along with edges and invoked graph

--- cohort/LangGraph/08_mem0_integration.py
+++ cohort/LangGraph/08_mem0_integration.py
@@ -2,7 +2,16 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 from mem0 import Memory
-import uuid
+
+from typing_extensions import TypedDict, Annotated
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.graph.message import add_messages
+from langgraph.graph import StateGraph, START, END
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
 
 mem_api_key = os.environ['MEM0_API_KEY']
 
@@ -33,3 +42,38 @@ config = {
 
 user_id = 'srikar'
 memory = Memory.from_config(config, user_id)
+
+# defining state:
+
+class MemoryState(TypedDict):
+    input: str
+    messages: Annotated[list, add_messages]
+
+# defining nodes : 
+
+def add_to_memory(state: MemoryState):
+    responses = state['messages']
+    result = '\n----\n'.join(response for response in responses)
+    print(result)
+
+def llm_call(state: MemoryState):
+    user_query = state['input']
+    result = model.invoke(user_query)
+    return {'messages': [{'role': 'system', 'content': result.content}]}
+
+graph_builder = StateGraph(MemoryState)
+
+graph_builder.add_node('llm_call', llm_call)
+graph_builder.add_node('add_to_memory', add_to_memory)
+
+graph_builder.add_edge(START, 'llm_call')
+graph_builder.add_edge('llm_call', 'add_to_memory')
+graph_builder.add_edge('add_to_memory', END)
+
+graph = graph_builder.compile()
+
+user_query = input('tell us about yourself : ')
+
+final_state = graph.invoke({
+    'input': user_query
+})
