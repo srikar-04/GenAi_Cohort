e3afb922e124ace7ab8d94f30472227f474bea59
srikar-04 <srikar.code01@gmail.com>
2025-07-21 11:07:37 +0530

    retreiving relevant memory and feeding to models context

--- cohort/LangGraph/08_mem0_integration.py
+++ cohort/LangGraph/08_mem0_integration.py
@@ -26,9 +26,10 @@ config = {
     'vector_store': {
         'provider': 'qdrant',
         'config': {
-            'collection_name': 'test',
+            'collection_name': 'user_info',
             'host': 'localhost',
-            'port': 6333
+            'port': 6333,
+            'embedding_model_dims': 768
         }
     },
     'embedder': {
@@ -38,10 +39,9 @@ config = {
             'embedding_dims': 768
         }
     },
-    "history_db_path": "./history.db",
+    # "history_db_path": "./history.db",
 }
 
-user_id = 'srikar'
 memory = Memory.from_config(config)
 
 # defining state:
@@ -49,32 +49,58 @@ memory = Memory.from_config(config)
 class MemoryState(TypedDict):
     input: str
     messages: Annotated[list, add_messages]
+    memory_info: str
 
 # defining nodes : 
 
 def add_to_memory(state: MemoryState):
     responses = state['messages']
-    result = '\n---------------\n'.join(response for response in responses)
-    print(result)
+    system_result = '\n---------------\n'.join(response.content for response in responses)
+    messages = [
+        {'role': 'user', 'content': state['input']},
+        {'role': 'system', 'content': state['messages'][-1].content}
+    ]
+    memory.add(messages, user_id='srikar')
+    # print(f"UPDATED MEMORY : {result}")
+    print(system_result)
+
+def retreive_memory(state: MemoryState):
+    user_query = state['input']
+    enitre_memory = memory.search(query=user_query, user_id='srikar')
+    relevant_memory = ''
+    if enitre_memory['results'] and len(enitre_memory['results']) != 0:
+        relevant_memory = "\n-----------\n".join(event.get('memory') for event in enitre_memory['results'])
+        # print(enitre_memory['results'])
+    return {'memory_info': relevant_memory}
 
 def llm_call(state: MemoryState):
     user_query = state['input']
-    result = model.invoke(user_query)
-    return {'messages': [{'role': 'system', 'content': result.content}]}
+    relevant_memory = state['memory_info']
+    result = model.invoke(f"""
+        You are an helpuful ai assistant, you can generate response based on the available info about the user. 
+        Here is what you know about the user : \n {relevant_memory} 
+        
+        Here is the query form the user :
+        {user_query}
+    """)
+    return {'messages': {'role': 'system', 'content': result.content}}
 
 graph_builder = StateGraph(MemoryState)
 
 graph_builder.add_node('llm_call', llm_call)
 graph_builder.add_node('add_to_memory', add_to_memory)
+graph_builder.add_node('retreive_memory', retreive_memory)
 
-graph_builder.add_edge(START, 'llm_call')
+graph_builder.add_edge(START, 'retreive_memory')
+graph_builder.add_edge('retreive_memory', 'llm_call')
 graph_builder.add_edge('llm_call', 'add_to_memory')
 graph_builder.add_edge('add_to_memory', END)
 
 graph = graph_builder.compile()
 
-user_query = input('tell us about yourself : ')
+while True:
+    user_query = input('> ')
 
-final_state = graph.invoke({
-    'input': user_query
-})
+    final_state = graph.invoke({
+        'input': user_query
+    })
