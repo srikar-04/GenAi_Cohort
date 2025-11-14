efe70be6b7fed0c8b6cc2eb52c6935493c0caba9
srikar-04 <srikar.code01@gmail.com>
2025-11-14 21:50:40 +0530

    installed langgraph

---
+++ Js/RAG/01_basicPdfRAG.js
@@ -0,0 +1,12 @@
+// load document by specifying the path
+    // if it is already loaded then it is present in the cache
+// split the document using chunking method you want
+    // if it is already splitted then no need to do again
+// embbed the splitted text
+// store the embeddings in vector db
+
+// embed the user query
+// search vector db using the user's query embeddings
+// feed it to the context of llm
+// generate final output
+
