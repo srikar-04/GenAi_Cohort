925e3e5c50910b1e4cbf05f5be2037160f3a54b6
srikar-04 <srikar.code01@gmail.com>
2025-11-16 22:56:54 +0530

    Added new dependencies for Google Generative AI and Qdrant, implemented vector store integration in 01_basicPdfRAG.js for document embedding and retrieval, and created a docker-compose file for Qdrant service.

--- Js/RAG/01_basicPdfRAG.js
+++ Js/RAG/01_basicPdfRAG.js
@@ -10,10 +10,18 @@
 // feed it to the context of llm
 // generate final output
 
+import dotenv from 'dotenv'
+dotenv.config()
+import OpenAI from 'openai'
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf"
 import path from 'path'
 import fs from 'fs'
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters"
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai"
+import { QdrantVectorStore } from "@langchain/qdrant"
+import { QdrantClient } from '@qdrant/js-client-rest'
+import readline from 'readline'
+
 
 const userDir = 'C:/Users/srikar/Desktop/RAG_Docs/leph1dd'
 
@@ -82,3 +90,112 @@ if(fs.existsSync(splittedCachePath)) {
     fs.writeFileSync(splittedCachePath, JSON.stringify(splitDocs, null, 2))
     console.log('✅ Chunks saved to cache:', splittedCachePath);
 }
+
+// (3) embedding the splitted text and storing it in vector DB :
+
+const embedder = new GoogleGenerativeAIEmbeddings({
+    model: "text-embedding-004"
+});
+
+const QdrantURL = "http://localhost:6333"
+const QdrantCollection = "NCERT_Physics_1"
+
+// check if there are any exsisting collections with embeddings 
+
+const Qclient = new QdrantClient({ host: "localhost", port: 6333 })
+
+const exsistingCollections = await Qclient.getCollections()
+const needIngestion = exsistingCollections.collections?.some(col => col.name === QdrantCollection)
+
+let vectorStore;
+
+if(!needIngestion) {
+    console.log('📊 storing data in new collection')
+    vectorStore = await QdrantVectorStore.fromDocuments(splitDocs, embedder, {
+        url: QdrantURL,
+        collectionName: QdrantCollection
+    })
+} else {
+    const collectionInfo = await Qclient.getCollection(QdrantCollection)
+    const isEmpty = collectionInfo.points_count === 0
+
+    if(isEmpty) {
+        console.log('📊 collection exists but is empty, adding documents')
+        vectorStore = await QdrantVectorStore.fromExistingCollection(embedder, {
+            url: QdrantURL,
+            collectionName: QdrantCollection
+        })
+        await vectorStore.addDocuments(splitDocs)
+    } else {
+        console.log('📊 collection exists with data, skipping documents addition')
+    }
+    // else {
+    //     console.log('📊 collection exists with data, adding more documents')
+    //     vectorStore = await QdrantVectorStore.fromExistingCollection(embedder, {
+    //         url: QdrantURL,
+    //         collectionName: QdrantCollection
+    //     })
+    //     await vectorStore.addDocuments(splitDocs)
+    // }
+}
+
+// (4) querying the vector store by turning it into a retireiver
+
+const retireiver = vectorStore.asRetriever()
+
+// const apiKey = process.env.GEMINI_API_KEY
+
+// if(!apiKey) throw new Error('api key not found')
+
+// const client = new OpenAI({
+//     apiKey: apiKey,
+//     baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/"
+// })
+
+// const messages = []
+
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
+
+
+// let realtedContext;
+
+// while (true) {
+
+//     realtedContext = ""
+    
+//     rl.question('🧠 Enter your qeury (or type "exit")', async (qeury) => {
+
+//         messages.push({
+//             role: "user",
+//             content: qeury
+//         })
+
+//         if(qeury.toLowerCase === 'exit') {
+//             rl.close()
+//         }
+
+//         // retireiving related docuemnts
+//         const relevantDocs = retireiver.invoke(qeury)
+//         relevantDocs.map(doc => realtedContext.concat("\n\n", doc.pageContent, "\n\n"))
+//     })
+// }
+
+// const systemPrompt = `
+//     Answert the use query only based on the context available which is delimented in triple quotes :
+//     """${realtedContext}""""
+
+//     If the questions does not relate to the available context then response with "no context available for this topic" 
+// `
+// messages.push({
+//     role: "developer",
+//     content: systemPrompt
+// })
+
+// const response = await client.chat.completions.create({
+//     model: "gemini-2.0-flash",
+//     messages: messages
+// })
+
