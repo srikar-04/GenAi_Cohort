aa6143135eeabf7ecbb810b3c278e33a7d1b195e
srikar-04 <srikar.code01@gmail.com>
2025-11-30 23:26:00 +0530

    initializing or creating a table for file Indexing

---
+++ Js/RAG/init_db.js
@@ -0,0 +1,27 @@
+// initialize the schema for the database for the 02_efficientFileHandling.js
+
+// we are using better-sqlite3
+
+// defining the schema
+
+import Database from 'better-sqlite3'
+
+const db = new Database('fileIndex.db')
+
+// creating table for file indexing (schema)
+
+db.prepare(`
+        CREATE TABLE IF NOT EXISTS fileIndex (
+            fileHash TEXT PRIMARY KEY,
+            fileName TEXT,
+            fileSize INTEGER,
+            uploadedAt TEXT,
+            updatedAt TEXT,
+            chunkCount INTEGER,
+            indexCount INTEGER
+        )
+    `).run()
+
+console.log('database initialized successfully')
+
+export default db
