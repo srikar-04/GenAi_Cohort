30fe5c453537c7b9b8973e44f5478b93cd611835
srikar-04 <srikar.code01@gmail.com>
2025-12-04 09:52:44 +0530

    feat: Implement an end-to-end RAG pipeline for efficient PDF processing, including deduplication, chunking, embedding, Qdrant vector storage, and an interactive LLM chat.

--- Js/RAG/02_efficientFileHandling.js
+++ Js/RAG/02_efficientFileHandling.js
@@ -11,6 +11,8 @@ import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
 import { QdrantClient } from "@qdrant/js-client-rest";
 import { QdrantVectorStore } from "@langchain/qdrant";
 import { fileURLToPath } from 'url';
+import readline from 'readline'
+import OpenAI from 'openai'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -20,11 +22,107 @@ dotenv.config({ path: path.resolve(__dirname, '../.env') });
 const apiKey = process.env.GEMINI_API_KEY;
 if (!apiKey) throw new Error('api key not found');
 
+const QdrantCollection = 'NCERT_Physics_2'
+const QdrantURL = "http://localhost:6333"
+
 function routeFile() {
 
 }
 
-async function chunkAndVector(pdfPath, fileHash) {
+async function llmCall(vectorStore) {
+    let messages = []
+
+    const client = new OpenAI({
+        apiKey: apiKey,
+        baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/"
+    })
+
+    // setting up readline
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    // helper function for asking questions
+    const askQuestion = (query) => {
+        return new Promise((resolve) => rl.question(query, resolve))
+    }
+
+        console.log('\n🤖 Chatbot ready! Type "exit" to quit.\n');
+    while(true) {
+        const userQuery = await askQuestion("🧠 Enter your query: ")
+
+        if(userQuery.toLowerCase === 'exit') {
+            console.log('\n🤖 Chatbot exiting.\n')
+            rl.close()
+            break
+        }
+
+        messages.push({
+            'role': 'user',
+            'content': userQuery
+        })
+
+        // extracting relevant context from the retriever 
+        console.log("🔍 Retrieving context... \n ");
+        const retriever = vectorStore.asRetriever()
+        let relevantContext;
+        try {
+            const relevantDocs = await retriever.invoke(userQuery)
+            relevantContext = relevantDocs.map(doc => doc.pageContent).join("\n\n \n\n")
+        } catch (error) {
+            console.log('error in retrieving context : ', error)
+            return
+        }
+
+        // writing system prompt
+        const systemPrompt = `
+        Answer the user query only based on the context available which is delimited in triple quotes :
+        """${relevantContext}"""
+
+        If the question does not relate to the available context then response with "no context available for this topic" 
+
+        If you think you have the related context to answer the question then answer it by breaking down the answer in simple terms and make sure not to include any extra information that is not related to the question or the context.`
+
+        const currentMessages = [
+            { role: "system", content: systemPrompt },
+            ...messages
+        ];
+
+        try {
+            const response = await client.chat.completions.create({
+                model: "gemini-2.0-flash",
+                messages: currentMessages
+            })
+
+            const answer = response.choices[0].message.content;
+            console.log(`\n🤖 Answer: ${answer}\n`);
+
+            messages.push({
+                role: "assistant",
+                content: answer
+            });
+
+        } catch (error) {
+            console.error("❌ Error generating response:", error.message);
+        }
+
+    }
+}
+
+async function getVectorStore() {
+    const embedder = new GoogleGenerativeAIEmbeddings({
+        apiKey: apiKey,
+        model: "text-embedding-004"
+    })
+
+    return await QdrantVectorStore.fromExistingCollection(embedder, {
+        url: QdrantURL,
+        collectionName: QdrantCollection
+    })
+}
+
+async function chunkAndVector(pdfPath, fileHash, vectorStore) {
     // 1) load the pdf from local disk
     let docs;
     if(fs.existsSync(pdfPath)) {
@@ -50,16 +148,13 @@ async function chunkAndVector(pdfPath, fileHash) {
         model: "text-embedding-004"
     })
 
-    const QdrantURL = "http://localhost:6333"
-    const QdrantCollection = "NCERT_Physics_2"
-
     const Qclient = new QdrantClient({host: 'localhost', port: 6333})
 
     // checking qdrant connection
     try {
         await Qclient.getCollections()
     } catch (error) {
-        console.log('could not connect to qdrant client ')
+        console.log('could not connect to qdrant client \n ')
         process.exit(1)
     }
 
@@ -67,32 +162,24 @@ async function chunkAndVector(pdfPath, fileHash) {
     const collectionExists = existingCollections.collections?.some(col => col.name === QdrantCollection)
 
     // 4) store the chunks in vector db
-    let vectorStore;
+    // getting vector store from from the place where we are calling function, in the main function we are getting it
+
     if(!collectionExists) {
-        console.log('📊 storing data in new collection')
+        console.log('📊 storing data in new collection \n')
         vectorStore = await QdrantVectorStore.fromDocuments(splittedDocs, embedder, {
             url: QdrantURL,
             collectionName: QdrantCollection
-        })
+        })     
     } else {
         const collectionInfo = await Qclient.getCollection(QdrantCollection)
         const isEmpty = collectionInfo.points_count === 0
 
         if(isEmpty) {
-            // creating an entirely new collection
-            vectorStore = await QdrantVectorStore.fromExistingCollection(embedder, {
-                url: QdrantURL,
-                collectionName: QdrantCollection
-            })
+            // adding docs to existing empty collection
             vectorStore.addDocuments(splittedDocs)
-        } else {
-            // collection exists with data, skipping documents addition
-            vectorStore = await QdrantVectorStore.fromExistingCollection(embedder, {
-                url: QdrantURL,
-                collectionName: QdrantCollection
-            })
-        }
+        } 
     }
+
 
     // 5) updating the file index -->> final step
     const entry = {
@@ -107,8 +194,7 @@ async function chunkAndVector(pdfPath, fileHash) {
 
     insertFileIndex(entry)
 
-    console.log('✅ file indexed successfully')
-    return
+    console.log('✅ file indexed successfully \n')
 }
 
 function hashBuffer(buffer) {
@@ -123,15 +209,15 @@ function hashBuffer(buffer) {
     try {
         fileIndex = getFileIndexById(fileHash)
     } catch (error) {
-        console.log('error while checking file index : ', error)
+        console.log('error while checking file index : ', error, '\n')
         return false
     }
 
     if(fileIndex) {
-        console.log('file is present in the index \n 🚀 skipping operations')
+        console.log('file is present in the index \n 🚀 skipping operations \n')
         return false
     } else {
-        console.log('file is not present in the index \n 🪓 performing operations')
+        console.log('file is not present in the index \n 🪓 performing operations \n ')
         return fileHash
     }
 }
@@ -147,10 +233,21 @@ function hashBuffer(buffer) {
         const pdfBuffer = fs.readFileSync(pdfPath);
         // 2) Hashing the buffer to avoid deduplication
         const result = hashBuffer(pdfBuffer)
+
+
+        // result is the fileHash that is returned from the hashBuffer function
+        // getting vector store and passing it to the chunkAndVector function
+        let vectorStore = await getVectorStore()
+
         if(result) {
-            await chunkAndVector(pdfPath, result)
+            await chunkAndVector(pdfPath, result, vectorStore)
         }
-        console.log('🤖 move to the ai query part')
+
+        if(vectorStore) {
+            await llmCall(vectorStore)
+        } else {
+            console.log('💀 vector store not found')
+        }
 
     } catch (error) {
         console.log('pipeline error : ', error)
